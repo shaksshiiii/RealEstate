5eb531de444f09a731be5d483906c339466d331c
shakshi singh <146609836+shaksshiiii@users.noreply.github.com>
2025-08-25 20:23:56 +0530

    Update App.jsx
    header=>Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/header';
+import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Testimonials from './components/Testimonials';
